91cbc16cfeed48c4f02fc64837da63f2e7848c1c
Ameen Oumer <ameen.oumer@gmail.com>
2025-10-16 08:47:09 +0300

    bug fixes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,30 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
 import {MatTableModule} from "@angular/material/table";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {FormsModule} from "@angular/forms";
 import {DataService} from "../data.service";
 import {MatMenuModule} from "@angular/material/menu";
-import {ActivatedRoute, Router, RouterLink} from "@angular/router";
+import {ActivatedRoute, RouterLink} from "@angular/router";
 import {environment} from "../../environments/environment";
-import {HttpClient} from "@angular/common/http";
 import {MatTableResponsiveModule} from "../mat-table-responsive/mat-table-responsive.module";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import * as jose from 'jose';
 import {AuthService} from '../auth/auth.service';
+import {NgOptimizedImage} from '@angular/common';
+import {MatDialog, MatDialogActions, MatDialogClose, MatDialogContent} from '@angular/material/dialog';
 
 @Component({
     selector: 'app-dashboard',
-    imports: [
-        MatTableModule,
-        MatIconModule,
-        MatButtonModule,
-        FormsModule,
-        MatMenuModule,
-        RouterLink,
-        MatTableResponsiveModule,
-        MatPaginatorModule,
-    ],
+  imports: [
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    MatMenuModule,
+    RouterLink,
+    MatTableResponsiveModule,
+    MatPaginatorModule,
+    NgOptimizedImage,
+  ],
     providers: [DataService],
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss'
@@ -33,18 +35,20 @@ export class DashboardComponent implements OnInit {
 
   constructor(private dataService: DataService,
               private route: ActivatedRoute,
-              private authService: AuthService,
-              private router: Router) { }
+              private authService: AuthService) { }
 
   baseUrl: string = environment.baseUrl;
   isAdmin!: boolean;
+  isNew!: boolean;
+  readonly dialog = inject(MatDialog);
 
   ngOnInit(): void {
     this.dataService.isAdmin$.subscribe(data => {
       this.isAdmin = data;
     });
-    this.route.queryParams.subscribe(params => {
-      const token = params['token'];
+    this.route.queryParams.subscribe((params) => {
+      const token: string = params['token'];
+      this.isNew = params['new'];
       if (token) {
         localStorage.setItem("access_token", token);
         this.authService.setLoginStatus(true);
@@ -52,13 +56,22 @@ export class DashboardComponent implements OnInit {
         if (decoded.sub) {
           localStorage.setItem('userId', <string>decoded["userId"]);
           localStorage.setItem('username', <string>decoded.sub);
-          localStorage.setItem('isSuper', <string>decoded['isSuperAdmin']);
+          localStorage.setItem('isSuper', 'false');
+          this.dataService.setIsAdmin(<boolean>decoded['isAdmin']);
         }
-        console.log('Login Successful. Token saved.');
-      } else {
-        this.router.navigate(['/login']).then(() => {return;});
       }
     });
+    if (this.isNew) {
+      this.dialog.open(WelcomeDialogComponent);
+    }
   }
-
 }
+
+@Component({
+  selector: 'welcome-dialog',
+  templateUrl: 'welcome-dialog.html',
+  styleUrls: ['./welcome-dialog.scss'],
+  imports: [MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class WelcomeDialogComponent { }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,23 +1,23 @@
-import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, inject, Input, OnInit} from '@angular/core';
 import {MatButtonModule} from "@angular/material/button";
 import {MatMenuModule} from "@angular/material/menu";
 import {MatDivider} from "@angular/material/divider";
 import {NgOptimizedImage, TitleCasePipe} from "@angular/common";
-import {MatBadge} from "@angular/material/badge";
 import {NavigationEnd, Router, RouterLink} from "@angular/router";
 import {AuthService} from "../auth/auth.service";
 import { HttpClient } from "@angular/common/http";
 import {environment} from "../../environments/environment";
-// import {Document, Notification} from "../interfaces";
-// import {PaymentDialog} from "../company-documents/document-dialog";
-// import {MatDialog} from "@angular/material/dialog";
+import {Notification} from "../interfaces";
+import {MatDialog} from "@angular/material/dialog";
 import {animate, state, style, transition, trigger} from "@angular/animations";
+import {MatBadge} from '@angular/material/badge';
+import {DocumentDialog} from '../documents/document-dialog';
 
 @Component({
     selector: 'app-nav',
     templateUrl: './nav.component.html',
     styleUrl: './nav.component.scss',
-  imports: [MatButtonModule, MatMenuModule, MatDivider, /*MatBadge,*/ TitleCasePipe, RouterLink, NgOptimizedImage],
+    imports: [MatButtonModule, MatMenuModule, MatDivider, MatBadge, TitleCasePipe, RouterLink, NgOptimizedImage],
     animations: [
         trigger('detailExpand', [
             state('opened', style({ visibility: 'visible', opacity: 1 })),
@@ -40,9 +40,9 @@ export class NavComponent implements OnInit, AfterViewInit {
   page!: string;
   baseUrl: string = environment.baseUrl;
   userId: string = `${localStorage.getItem('userId')}`;
-  // notifications: Notification[] = [];
-  // document!: Document;
-  // dialog = inject(MatDialog);
+  notifications: Notification[] = [];
+  document!: Document;
+  dialog = inject(MatDialog);
   isNavOpen: boolean = false;
   navbar: HTMLElement | null = null;
 
@@ -50,34 +50,33 @@ export class NavComponent implements OnInit, AfterViewInit {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         const url = this.router.url.split('?')[0].substring(1).split('/')[0];
-         if (url === 'dashboard' || url === 'adminDashboard') {
+         if (url === 'dashboard') {
            this.page = "Dashboard";
-         } else if (url === 'faq' || url === 'adminFAQ') {
-           this.page = "FAQ";
-         } else if (url === 'notifications' || url === 'adminNotification') {
+         } else if (url === 'notifications') {
            this.page = "Notification";
-         } else if (url === 'send-invoices' || url === 'send-documents' || url === 'company-documents') {
+         } else if (url === 'documents') {
            this.page = this.router.url.split('?')[0].substring(1).split('/')[0].replace('-', ' ');
          } else {
              this.page = this.router.url.split('?')[0].substring(1).split('/')[0];
          }
       }
     });
-    // this.http.get<Notification[]>(this.baseUrl + `notification/user/unread/${(this.userId)}`)
-    //   .subscribe((data) => {
-    //     if (data.length) {
-    //       data.sort((a, b) => {
-    //         if (a.date > b.date) return -1;
-    //         if (a.date < b.date) return 1;
-    //         return 0;
-    //       });
-    //       this.notifications = data;
-    //     }
-    //   });
+    this.http.get<Notification[]>(this.baseUrl + `notification/user/unread/${(this.userId)}`)
+      .subscribe((data) => {
+        if (data.length) {
+          data.sort((a, b) => {
+            if (a.date > b.date) return -1;
+            if (a.date < b.date) return 1;
+            return 0;
+          });
+          this.notifications = data;
+        }
+      });
   }
 
   ngAfterViewInit() {
     this.navbar = document.querySelector(".navigation");
+    this.username = `${localStorage.getItem('username')}`;
   }
 
   logOut() {
@@ -86,51 +85,42 @@ export class NavComponent implements OnInit, AfterViewInit {
     this.router.navigate(['login']).then(() => {return;})
   }
 
-  // openDialog(documentId: number) {
-  //   if (!this.document) {
-  //     this.http.get<Document>(this.baseUrl + `document/${documentId}`)
-  //       .subscribe((data) => {
-  //         if (data) {
-  //           this.document = data;
-  //           this.dialog.open(PaymentDialog, {
-  //             data: { document: this.document },
-  //             minWidth: 700,
-  //           });
-  //         }
-  //       });
-  //   } else {
-  //     this.dialog.open(PaymentDialog, {
-  //       data: { document: this.document },
-  //       minWidth: 700
-  //     });
-  //   }
-  // }
+  openDialog(documentId: number) {
+    if (!this.document) {
+      this.http.get<Document>(this.baseUrl + `document/${documentId}`)
+        .subscribe((data) => {
+          if (data) {
+            this.document = data;
+            this.dialog.open(DocumentDialog, {
+              data: { document: this.document },
+              minWidth: 700,
+            });
+          }
+        });
+    } else {
+      this.dialog.open(DocumentDialog, {
+        data: { document: this.document },
+        minWidth: 700
+      });
+    }
+  }
 
-  // viewNotification(notification: Notification, index: number) {
-  //   switch (notification.title) {
-  //     case 'Expiration Notification':
-  //     case 'New Document Received':
-  //       // this.openDialog(notification.item);
-  //       break;
-  //     case 'New User':
-  //       this.router.navigate(['/','users', 'view', notification.item])
-  //         .then(() => {return;})
-  //       break;
-  //     case 'New Order Received':
-  //       this.router.navigate(['/','orders', notification.item])
-  //         .then(() => {return;})
-  //       break;
-  //     case 'Document Request':
-  //       this.router.navigate(['/','document', 'request'])
-  //         .then(() => {return;})
-  //       break;
-  //     case 'Payment Received':
-  //       break;
-  //   }
-  //   this.http.patch(this.baseUrl + `notification/${notification.id}`, {})
-  //     .subscribe();
-  //   this.notifications.splice(index, 1);
-  // }
+  viewNotification(notification: Notification, index: number) {
+    switch (notification.title) {
+      case 'New Document Received':
+        this.openDialog(notification.item);
+        break;
+      case 'New User':
+        this.router.navigate(['/','users', 'view', notification.item])
+          .then(() => {return;})
+        break;
+      case 'Payment Received':
+        break;
+    }
+    this.http.patch(this.baseUrl + `notification/${notification.id}`, {})
+      .subscribe();
+    this.notifications.splice(index, 1);
+  }
 
   toggleNavbar() {
     this.isNavOpen = !this.isNavOpen;
